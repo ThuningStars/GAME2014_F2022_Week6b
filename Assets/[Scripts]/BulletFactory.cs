bb822afc52599286a7f11b86680a932c173a1145
Wanbo.Wang <wanbo.wang@georgebrown.ca>
2022-10-16 15:41:30 -0400

    lab4 finished

--- Assets/[Scripts]/BulletFactory.cs
+++ Assets/[Scripts]/BulletFactory.cs
@@ -16,7 +16,7 @@ public class BulletFactory : MonoBehaviour
     private Transform bulletParent;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         Initialize();
     }
--- Assets/[Scripts]/BulletManager.cs
+++ Assets/[Scripts]/BulletManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 [System.Serializable]
 public class BulletManager : MonoBehaviour
